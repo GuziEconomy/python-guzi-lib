32f1d65cf4fe34fc75056e0dd03eba263c590e5c
gdubus <gdubus@norauto.com>
2020-11-29 23:01:28 +0100

    Create zip/unzip tools functions to simplify saving

--- guzilib/utils.py
+++ guzilib/utils.py
@@ -2,9 +2,8 @@ import hashlib
 import ecdsa
 import umsgpack
 from enum import Enum
-
-
-
+from datetime import date
+import itertools
 
 EMPTY_HASH = 0
 VERSION = 1
@@ -13,6 +12,33 @@ MAX_TX_IN_BLOCK = 30
 
 def guzi_hash(data):
     return hashlib.sha256(data).digest()
+
+def zip_positions(positions):
+    # 1. [("2020-12-22",0),("2020-12-22",1),("2020-12-23",0),("2020-12-23",1)]
+    # => [(['2020-12-22'], [0, 1]), (['2020-12-23'], [0, 1])]
+    tmp_result = []
+    for key, group in itertools.groupby(positions, lambda x: x[0] ): 
+        tmp_result += [(key, [g[1] for g in group])]
+
+    # 2. [(['2020-12-22'], [0, 1]), (['2020-12-23'], [0, 1])]
+    # => [(['2020-12-22', '2020-12-23'], [0, 1])]
+    result = []
+    for key, group in itertools.groupby(tmp_result, lambda x: x[1]): 
+        result += [([g[0] for g in group], key)]
+
+    return result
+
+
+def unzip_positions(positions):
+    """
+    From [(['2020-12-22'], [0]),(['2020-12-23'], [0, 1]),(['2020-12-24'], [0])]
+    To   [("2020-12-22",0),("2020-12-23",0),("2020-12-23",1),("2020-12-24",0)]
+    """
+    result = []
+    for p in positions:
+        result += itertools.product(p[0], p[1])
+    result.sort()
+    return result
 
 
 class TxType(Enum):
@@ -41,6 +67,10 @@ class FullBlockError(GuziError):
 class NotRemovableTransactionError(GuziError):
     pass
 class InvalidBlockchainError(GuziError):
+    pass
+class NegativeAmountError(GuziError):
+    pass
+class InsufficientFundsError(GuziError):
     pass
 
 
---
+++ tests/test_utils.py
@@ -0,0 +1,94 @@
+from guzilib.utils import zip_positions, unzip_positions
+
+class TestZipPositions:
+    def test_zip_empty(self):
+        unzipped = []
+        expected = []
+
+        result = zip_positions(unzipped)
+
+        assert result == expected
+
+    def test_zip_positions_empty(self):
+        unzipped = [("2020-12-22",0)]
+        expected = [(['2020-12-22'], [0])]
+
+        result = zip_positions(unzipped)
+
+        assert result == expected
+
+    def test_zip_positions_less_than_5(self):
+        unzipped = [("2020-12-22",0),("2020-12-22",1),("2020-12-22",2),("2020-12-22",3),("2020-12-22",4)]
+        expected = [(['2020-12-22'], [0, 1, 2, 3, 4])]
+
+        result = zip_positions(unzipped)
+
+        assert result == expected
+
+    # TODO
+    #def test_zip_positions_more_than_5(self):
+    #    unzipped = [("2020-12-22",0),("2020-12-22",1),("2020-12-22",2),("2020-12-22",3),("2020-12-22",4), ("2020-12-22",5)]
+    #    expected = [(['2020-12-22'], [0, "to", 5])]
+
+    #    result = zip_positions(unzipped)
+
+    #    assert result == expected
+
+    #def test_zip_positions_more_than_5(self):
+    #    unzipped = [("2020-12-22",0),("2020-12-23",0),("2020-12-24",0)]
+    #    expected = [(['2020-12-22','to','2020-12-24'], [0])]
+
+    #    result = zip_positions(unzipped)
+
+    #    assert result == expected
+    
+    def test_zip_positions_dates(self):
+        unzipped = [("2020-12-22",0),("2020-12-23",0),("2020-12-24",0)]
+        expected = [(['2020-12-22','2020-12-23','2020-12-24'], [0])]
+
+        result = zip_positions(unzipped)
+
+        assert result == expected
+    
+    def test_zip_positions_only_when_possible(self):
+        unzipped = [("2020-12-22",0),("2020-12-23",0),("2020-12-23",1),("2020-12-24",0)]
+        expected = [(['2020-12-22'], [0]),(['2020-12-23'], [0, 1]),(['2020-12-24'], [0])]
+
+        result = zip_positions(unzipped)
+
+        assert result == expected
+
+
+class TestUnzipPositions:
+
+    def test_empty_case(self):
+        zipped = []
+        expected = []
+
+        result = unzip_positions(zipped)
+
+        assert result == expected
+
+    def test_base_case(self):
+        zipped = [(['2020-12-22'], [0])]
+        expected = [("2020-12-22",0)]
+
+        result = unzip_positions(zipped)
+
+        assert result == expected
+
+    def test_real_case(self):
+        zipped = [(['2020-12-22'], [0]),(['2020-12-23'], [0, 1]),(['2020-12-24'], [0])]
+        expected = [("2020-12-22",0),("2020-12-23",0),("2020-12-23",1),("2020-12-24",0)]
+
+        result = unzip_positions(zipped)
+
+        assert result == expected
+
+    def test_result_is_date_ordered(self):
+        zipped = [(['2020-12-24'], [0]),(['2020-12-23'], [0, 1]),(['2020-12-22'], [0])]
+        expected = [("2020-12-22",0),("2020-12-23",0),("2020-12-23",1),("2020-12-24",0)]
+
+        result = unzip_positions(zipped)
+
+        assert result == expected
